a68ccac160fa57ab467689e21d629f58693ce345
8c8c8c8c8c8 <8c8c8c8c8c8@gmail.com>
2026-03-28 19:16:55 +0900

    feat: 공통 설정
    --- content ---
    - issue, PR template
    - java google style
    - gradle
    - readme.md
    - .gitignore, .editorconfig, .gitmessage.txt

---
+++ src/main/java/com/fow/pentakode/Pentakode.java
@@ -0,0 +1,12 @@
+package com.fow.pentakode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pentakode {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Pentakode.class, args);
+  }
+}
